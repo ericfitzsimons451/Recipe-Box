8188cb94be43bf5790e0e29a166ec59d338b855f
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-12-12 14:43:56 -0700

    initial setup with successful fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: {},
+      searchResults: []
+    }
+  }
+
+  async componentDidMount() {
+    let query = 'chicken'
+    const response = await fetch(`https://recipe-puppy.p.rapidapi.com/?p=1&i=${query}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+        "x-rapidapi-key": "47c203197cmshefbf7a91d49503bp127312jsna17beb6191c9"
+      }
+    })
+    const data = await response.json()
+    this.setState({searchResults: data.results})
+    console.log(this.state.searchResults)
+  }
+
+
+  render() {
+    return(
+      <div className="App">
+        {this.state.searchResults.map(result => <img src={result.thumbnail} alt="food" />)}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
